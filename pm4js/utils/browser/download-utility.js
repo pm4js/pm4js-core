3b0ba58c66990da750d6d194ef118d74706a7652
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-06-24 11:45:28 +0200

    added example for XES exporting
    added example for XES exporting

--- pm4js/utils/browser/download-utility.js
+++
@@ -1,7 +0,0 @@
-function download(text, name, type) {
-  var a = document.createElement("a");
-  var file = new Blob([text], {type: type});
-  a.href = URL.createObjectURL(file);
-  a.download = name;
-  a.click();
-}
---
+++ pm4js/utils/browser/download.js
@@ -0,0 +1,9 @@
+class DownloadUtility {
+	static download(text, name, type) {
+	  var a = document.createElement("a");
+	  var file = new Blob([text], {type: type});
+	  a.href = URL.createObjectURL(file);
+	  a.download = name;
+	  a.click();
+	}
+}
