b8bb9fca1056f7eb7ef17f3706d16a10d84c1836
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-07-06 09:38:25 +0200

    intermediate commit
    intermediate commit

---
+++ pm4js/objects/conversion/process_tree/to_petri_net.js
@@ -0,0 +1,61 @@
+class ProcessTreeToPetriNetConverter {
+	static apply(processTree) {
+		let nodes = ProcessTreeToPetriNetConverter.orderBottomUpNodes(processTree);
+		let petriNet = new PetriNet();
+		let im = new Marking();
+		let fm = new Marking();
+		let sourcePlaces = {};
+		let targetPlaces = {};
+		let acceptingPetriNet = new AcceptingPetriNet(petriNet, im, fm);
+		return acceptingPetriNet;
+	}
+	
+	static sortNodes(nodes) {
+		let cont = true;
+		while (cont) {
+			cont = false;
+			let i = 0;
+			while (i < nodes.length - 1) {
+				let j = i + 1;
+				while (j < nodes.length) {
+					if (nodes[j].parentNode == nodes[i]) {
+						cont = true;
+						let temp = nodes[i];
+						nodes[i] = nodes[j];
+						nodes[j] = temp;
+					}
+					j++;
+				}
+				i++;
+			}
+		}		
+		return nodes;
+	}
+	
+	static orderBottomUpNodes(processTree) {
+		let descendants = {};
+		ProcessTreeToPetriNetConverter.findAllDescendants(processTree, descendants);
+		let nodes = Object.values(descendants);
+		nodes = ProcessTreeToPetriNetConverter.sortNodes(nodes);
+		return nodes;
+	}
+	
+	static findAllDescendants(processTree, descendants) {
+		descendants[processTree.id] = processTree;
+		for (let child of processTree.children) {
+			ProcessTreeVanillaVisualizer.findAllDescendants(child, descendants);
+		}
+	}
+}
+
+try {
+	require('../../../pm4js.js');
+	require('../../process_tree/process_tree.js');
+	require('../../petri_net/petri_net.js');
+	module.exports = {ProcessTreeToPetriNetConverter: ProcessTreeToPetriNetConverter};
+	global.ProcessTreeToPetriNetConverter = ProcessTreeToPetriNetConverter;
+}
+catch (err) {
+	console.log(err);
+	// not in Node
+}
