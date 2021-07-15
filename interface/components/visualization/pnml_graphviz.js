88ade47e92d00d9aeca8dcfe0ab24501fcbfdac6
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-07-15 11:59:52 +0200

    added visualization for TBR
    added visualization for TBR

--- interface/components/visualization/pnml_graphviz.js
+++ interface/components/visualization/pnml_graphviz.js
@@ -4,6 +4,13 @@ class PnmlGraphvizVanilla {
 		let svgXml = Viz(gv, { format: "svg"});
 		document.getElementById(component).innerHTML = svgXml;
 	}
+	
+	static applyFrequency(tbrResults, component) {
+		let gv = PetriNetFrequencyVisualizer.apply(tbrResults.acceptingPetriNet, tbrResults);
+		let svgXml = Viz(gv, { format: "svg"});
+		document.getElementById(component).innerHTML = svgXml;
+	}
 }
 
 Pm4JS.registerVisualizer("PnmlGraphvizVanilla", "applyVanilla", "AcceptingPetriNet", "Show Accepting Petri Net (Graphviz; vanilla)", "Alessandro Berti");
+Pm4JS.registerVisualizer("PnmlGraphvizVanilla", "applyFrequency", "TokenBasedReplayResult", "Show Accepting Petri net decorated with TBR results", "Alessandro Berti");
