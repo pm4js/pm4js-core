5e3af5bbfb41ae3366d353d29cefd70b4c4c2d71
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-07-16 09:46:28 +0200

    added simplicity calculation
    added simplicity calculation

--- init.js
+++ init.js
@@ -19,6 +19,7 @@ require('./pm4js/algo/filtering/log/general.js');
 require('./pm4js/algo/discovery/flower/algorithm.js');
 require('./pm4js/algo/conformance/tokenreplay/algorithm.js');
 require('./pm4js/algo/evaluation/generalization/tbr_result.js');
+require('./pm4js/algo/evaluation/simplicity/arc_degree.js');
 require('./pm4js/objects/dfg/frequency/obj.js');
 require('./pm4js/objects/dfg/performance/obj.js');
 require('./pm4js/algo/discovery/dfg/algorithm.js');
---
+++ pm4js/algo/evaluation/simplicity/arc_degree.js
@@ -0,0 +1,45 @@
+class SimplicityArcDegreeResults {
+	constructor(value) {
+		this.value = value;
+	}
+}
+
+class SimplicityArcDegree {
+	static apply(acceptingPetriNet, k=0) {
+		let summ = 0.0;
+		let count = 0;
+		for (let placeId in acceptingPetriNet.net.places) {
+			let place = acceptingPetriNet.net.places[placeId];
+			summ += Object.keys(place.inArcs).length;
+			summ += Object.keys(place.outArcs).length;
+			count += 2;
+		}
+		for (let transId in acceptingPetriNet.net.transitions) {
+			let trans = acceptingPetriNet.net.transitions[transId];
+			summ += Object.keys(trans.inArcs).length;
+			summ += Object.keys(trans.outArcs).length;
+			count += 2;
+		}
+		let meanDegree = 0;
+		if (count > 0) {
+			meanDegree = summ / count;
+		}
+		let simplicity = 1.0 / (1.0 + Math.max(meanDegree - k, 0));
+		let ret = new SimplicityArcDegreeResults(simplicity);
+		return ret;
+	}
+}
+
+try {
+	require("../../../pm4js.js");
+	require("../../../objects/petri_net/petri_net.js");
+	module.exports = {SimplicityArcDegree: SimplicityArcDegree, SimplicityArcDegreeResults: SimplicityArcDegreeResults};
+	global.SimplicityArcDegree = SimplicityArcDegree;
+	global.SimplicityArcDegreeResults = SimplicityArcDegreeResults;
+}
+catch (err) {
+	// not in Node
+	console.log(err);
+}
+
+Pm4JS.registerAlgorithm("SimplicityArcDegree", "apply", ["AcceptingPetriNet"], "SimplicityArcDegreeResults", "Calculate Simplicity (Arc Degree)", "Alessandro Berti");
