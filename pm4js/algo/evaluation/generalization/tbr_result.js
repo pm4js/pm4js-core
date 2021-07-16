65283d08734235da0c5b841e365b60078efcc6eb
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-07-16 09:30:31 +0200

    added calculation of generalization
    added calculation of generalization

---
+++ pm4js/algo/evaluation/generalization/tbr_result.js
@@ -0,0 +1,32 @@
+class GeneralizationTbrResults {
+	constructor(value) {
+		this.value = value;
+	}
+}
+
+class GeneralizationTbr {
+	static apply(tbrResults) {
+		let invSqOccSum = 0.0
+		for (let trans in tbrResults.transExecutions) {
+			let thisTerm = 1.0 / Math.sqrt(Math.max(tbrResults.transExecutions[trans], 1));
+			invSqOccSum += thisTerm;
+		}
+		let ret = new GeneralizationTbrResults(1.0 - invSqOccSum/(Object.keys(tbrResults.transExecutions).length));
+		Pm4JS.registerObject(ret, "Generalization Results");
+		return ret;
+	}
+}
+
+try {
+	require("../../../pm4js.js");
+	require("../../conformance/tokenreplay/algorithm.js");
+	module.exports = {GeneralizationTbr: GeneralizationTbr, GeneralizationTbrResults: GeneralizationTbrResults};
+	global.GeneralizationTbr = GeneralizationTbr;
+	global.GeneralizationTbrResults = GeneralizationTbrResults;
+}
+catch (err) {
+	// not in Node
+	console.log(err);
+}
+
+Pm4JS.registerAlgorithm("GeneralizationTbr", "apply", ["TokenBasedReplayResult"], "GeneralizationTbrResults", "Calculate Generalization", "Alessandro Berti");
