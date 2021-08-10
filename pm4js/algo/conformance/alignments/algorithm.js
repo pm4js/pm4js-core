aacff1a12cb445814f3036278645df313457bccf
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-08-10 14:10:09 +0200

    revision

--- pm4js/algo/conformance/alignments/algorithm.js
+++ pm4js/algo/conformance/alignments/algorithm.js
@@ -12,7 +12,7 @@ class PetriNetAlignments {
 			for (let transId in acceptingPetriNet.net.transitions) {
 				let trans = acceptingPetriNet.net.transitions[transId];
 				if (trans.label == null) {
-					modelMoveCosts[transId] = 1;
+					modelMoveCosts[transId] = 0;
 				}
 				else {
 					modelMoveCosts[transId] = 10000;
@@ -63,15 +63,29 @@ class PetriNetAlignments {
 			}
 			res.push(alignedTraces[listAct]);
 			console.log(alignedTraces[listAct]);
-			break;
 		}
 		return res;
+	}
+	
+	static checkClosed(closedSet, tup) {
+		if (tup[3] in closedSet && tup[2] <= closedSet[tup[3]]) {
+			return true;
+		}
+		return false;
+	}
+	
+	static closeTuple(closedSet, tup) {
+		if (!(tup[3] in closedSet)) {
+			closedSet[tup[3]] = tup[2];
+		}
+		closedSet[tup[3]] = Math.max(tup[2], closedSet[tup[3]]);
 	}
 	
 	static applyTrace(listAct, net, im, fm, syncCosts, modelMoveCosts, logMoveCosts, comparator) {
 		let queue = new PriorityQueue(comparator);
 		queue.push([0, 0, 0, im, false, null, null]);
 		let count = 0;
+		let closedSet = {};
 		while (true) {
 			count++;
 			let tup = queue.pop();
@@ -79,9 +93,13 @@ class PetriNetAlignments {
 				return null;
 			}
 			else if (tup[3].equals(fm) && tup[1] == listAct.length) {
-				return tup;
+				return PetriNetAlignments.formAlignment(listAct, tup);
+			}
+			else if (PetriNetAlignments.checkClosed(closedSet, tup)) {
+				continue;
 			}
 			else {
+				PetriNetAlignments.closeTuple(closedSet, tup);
 				if (!(tup[3].equals(fm))) {
 					let enabledTransitions = tup[3].getEnabledTransitions();
 					for (let trans of enabledTransitions) {
@@ -89,20 +107,50 @@ class PetriNetAlignments {
 						if (tup[1] < listAct.length && trans.label == listAct[tup[1]]) {
 							// sync move
 							newTup = [tup[0] + syncCosts[trans.toString()], tup[1] + 1, count, tup[3].execute(trans), false, trans, tup];
-							queue.push(newTup);
+							if (!(PetriNetAlignments.checkClosed(closedSet, newTup))) {
+								queue.push(newTup);
+							}
 						}
 						else {
 							// move on model
 							newTup = [tup[0] + modelMoveCosts[trans.toString()], tup[1], count, tup[3].execute(trans), true, trans, tup];
-							queue.push(newTup);
+							if (!(PetriNetAlignments.checkClosed(closedSet, newTup))) {
+								queue.push(newTup);
+							}
 						}
 					}
 				}
 				if (tup[1] < listAct.length && !(tup[4])) {
-					queue.push([tup[0] + logMoveCosts[listAct[tup[1]]], tup[1] + 1, count, tup[3], false, null, tup]);
+					let newTup = [tup[0] + logMoveCosts[listAct[tup[1]]], tup[1] + 1, count, tup[3], false, null, tup]
+					if (!(PetriNetAlignments.checkClosed(closedSet, newTup))) {
+						queue.push(newTup);
+					}
 				}
 			}
 		}
+	}
+	
+	static formAlignment(listAct, tup) {
+		let ret = [];
+		let cost = tup[0];
+		let closedStates = tup[2];
+		while (tup[6] != null) {
+			let isMM = tup[4];
+			let currTrans = tup[5];
+			if (currTrans == null) {
+				// lm
+				ret.push("("+listAct[tup[1]-1]+",>>)")
+			}
+			else if (isMM) {
+				ret.push("(>>,"+currTrans.name+")")
+			}
+			else {
+				ret.push("("+listAct[tup[1]-1]+","+currTrans.name+")")
+			}
+			tup = tup[6];
+		}
+		ret.reverse();
+		return {"alignment": ret.join(","), "cost": cost, "closedStates": closedStates}
 	}
 }
 
