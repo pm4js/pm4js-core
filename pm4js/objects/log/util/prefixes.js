07a47846dcc258a7780df98f75a37d76b70e08e4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-08-18 13:57:52 +0200

    added utility to retrieve all the prefixes from the log
    added utility to retrieve all the prefixes from the log

---
+++ pm4js/objects/log/util/prefixes.js
@@ -0,0 +1,35 @@
+class EventLogPrefixes {
+	static apply(eventLog, activityKey="concept:name") {
+		let prefixes = {};
+		let i = 0;
+		for (let trace of eventLog.traces) {
+			i = 0;
+			let actArray = [];
+			while (i < trace.events.length - 1) {
+				let act = trace.events[i].attributes[activityKey].value;
+				let nextAct = trace.events[i+1].attributes[activityKey].value;
+				actArray.push(act);
+				if (!(actArray in prefixes)) {
+					prefixes[actArray] = {};
+				}
+				if (!(nextAct in prefixes[actArray])) {
+					prefixes[actArray][nextAct] = 0;
+				}
+				prefixes[actArray][nextAct] += 1;
+				i++;
+			}
+		}
+		return prefixes;
+	}
+}
+
+try {
+	require('../../../pm4js.js');
+	require('../log.js');
+	module.exports = {EventLogPrefixes: EventLogPrefixes};
+	global.EventLogPrefixes = EventLogPrefixes;
+}
+catch (err) {
+	// not in node
+	console.log(err);
+}
