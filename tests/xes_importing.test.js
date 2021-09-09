c7e618c52f90fc497fbdebccc32656f4f6a31da0
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-09 07:47:26 +0200

    XES importing test
    XES importing test

--- tests/xes_importing.test.js
+++ tests/xes_importing.test.js
@@ -1,8 +1,23 @@
 require("../init.js");
 var fs = require('fs');
+const os = require('os');
 
 test("XES importing running-example", () => {
-	fs.readFile('examples/input_data/running-example.xes', {encoding: 'utf-8'}, (err, data) => {
-		let eventLog = XesImporter.apply(data);
-	});
+	let data = fs.readFileSync('examples/input_data/running-example.xes', {encoding: 'utf-8'});
+	let eventLog = XesImporter.apply(data);
+	let xmlStri = XesExporter.apply(eventLog);
+	let fileName = os.tmpdir() + "/" + "running-example.xes";
+	fs.writeFileSync(fileName, xmlStri);
+	data = fs.readFileSync(fileName, {encoding: 'utf-8'});
+	eventLog = XesImporter.apply(data);
+});
+
+test("XES importing receipt", () => {
+	let data = fs.readFileSync('examples/input_data/receipt.xes', {encoding: 'utf-8'});
+	let eventLog = XesImporter.apply(data);
+	let xmlStri = XesExporter.apply(eventLog);
+	let fileName = os.tmpdir() + "/" + "receipt.xes";
+	fs.writeFileSync(fileName, xmlStri);
+	data = fs.readFileSync(fileName, {encoding: 'utf-8'});
+	eventLog = XesImporter.apply(data);
 });
