d0296545e76321b0b93f083a236f7cfcba57d4ec
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-13 11:03:24 +0200

    removed file

--- playground.js
+++
@@ -1,9 +0,0 @@
-require("./init.js");
-var fs = require('fs');
-const os = require('os');
-
-let data = fs.readFileSync('examples/input_data/running-example.ptml', {encoding: 'utf-8'});
-let processTree = PtmlImporter.apply(data);
-let xmlStri = PtmlExporter.apply(processTree);
-console.log(xmlStri);
-fs.writeFileSync("prova.ptml", xmlStri);
