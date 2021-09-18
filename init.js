f3acc7611baba5f15fe590ce20313c03af460d3a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-18 09:53:13 +0200

    Petri net playout
    Petri net playout

--- init.js
+++ init.js
@@ -52,3 +52,4 @@ require('./pm4js/objects/conversion/wf_net/to_bpmn.js');
 require('./pm4js/objects/process_tree/exporter/exporter.js');
 require('./pm4js/objects/dfg/exporter/exporter.js');
 require('./pm4js/objects/dfg/importer/importer.js');
+require('./pm4js/algo/simulation/playout/petri_net/algorithm.js');
---
+++ pm4js/algo/simulation/playout/petri_net/algorithm.js
@@ -0,0 +1,43 @@
+class PetriNetPlayout {
+	static apply(acceptingPetriNet, numDesideredTraces=1000, activityKey="concept:name", timestampKey="time:timestamp") {
+		let petriNet = acceptingPetriNet.net;
+		let initialMarking = acceptingPetriNet.im;
+		let finalMarking = acceptingPetriNet.fm;
+		let count = 0;
+		let minTimestamp = 10000000;
+		let eventLog = new EventLog();
+		while (true) {
+			if (count >= numDesideredTraces) {
+				break;
+			}
+			let trace = new Trace();
+			let marking = initialMarking.copy();
+			while (!(finalMarking.equals(marking))) {
+				let enabledTransitions = marking.getEnabledTransitions();
+				let pickedTransition = enabledTransitions[Math.floor(Math.random() * enabledTransitions.length)];
+				if (pickedTransition.label != null) {
+					let eve = new Event();
+					eve.attributes[activityKey] = new Attribute(pickedTransition.label);
+					eve.attributes[timestampKey] = new Attribute(new Date(minTimestamp*1000));
+					trace.events.push(eve);
+				}
+				marking = marking.execute(pickedTransition);
+				minTimestamp++;
+			}
+			eventLog.traces.push(trace);
+			count++;
+		}
+		Pm4JS.registerObject(eventLog, "Simulated Event log (from Petri net)");
+		return eventLog;
+	}
+}
+
+try {
+	require('../../../../pm4js.js');
+	global.PetriNetPlayout = PetriNetPlayout;
+	module.exports = {PetriNetPlayout: PetriNetPlayout};
+}
+catch (err) {
+	// not in Node
+	//console.log(err);
+}
