f584725be4dd7ffff8f26a49e4db0d98421b3a87
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-21 11:37:34 +0200

    added tests on the random variables
    added tests on the random variables

--- tests/random_variables.test.js
+++ tests/random_variables.test.js
@@ -55,3 +55,26 @@ test("gamma random variable", () => {
 	let pdfMean = rv.pdf(rv.getMean());
 	let simValue = 	rv.getValue();
 });
+
+test("uniform random variable", () => {
+	let rv = UniformRandomVariable.estimateParameters([1.9, 2, 2.1]);
+	let rvMean = rv.getMean();
+	let rvVariance = rv.getVariance();
+	let rvMode = rv.getMode();
+	let pdfMean = rv.pdf(rv.getMean());
+	let cdfMean = rv.cdf(rv.getMean());
+	let pdfMedian = rv.pdf(rv.getMedian());
+	let cdfMedian = rv.cdf(rv.getMedian());
+	let quantile5 = rv.getQuantile(0.5);
+	let quantile75 = rv.getQuantile(0.75);
+	let quantile9 = rv.getQuantile(0.9);
+	let simValue = 	rv.getValue();
+});
+
+test("exponentially modified gaussian random variable", () => {
+	let rv = ExponentiallyModifiedGaussian.estimateParameters([1.9, 2, 2.1]);
+	let rvMean = rv.getMean();
+	let rvVariance = rv.getVariance();
+	let pdfMean = rv.pdf(rv.getMean());
+	let simValue = 	rv.getValue();
+});
