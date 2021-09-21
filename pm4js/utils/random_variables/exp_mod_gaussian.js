8b6036f37f419faf5177321ef44f88a2b3f4cafc
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-21 10:31:14 +0200

    added missing functions to the exponentially modified gaussian
    added missing functions to the exponentially modified gaussian

--- pm4js/utils/random_variables/exp_mod_gaussian.js
+++ pm4js/utils/random_variables/exp_mod_gaussian.js
@@ -53,6 +53,32 @@ class ExponentiallyModifiedGaussian {
 	getVariance() {
 		return this.sig * this.sig + 1.0 / (this.lam * this.lam);
 	}
+	
+	pdf(x) {
+		return this.lam / 2.0 * Math.exp(this.lam / 2.0 * (2 * this.mu + this.lam * this.sig * this.sig - 2*x)) * (1 - ExponentiallyModifiedGaussian.erf((this.mu + this.lam * this.sig * this.sig - x)/(Math.sqrt(2) * this.sig)));
+	}
+	
+	cdf(x) {
+		let u = this.lam * (x - this.mu);
+		let v = this.lam * this.sig;
+		let rv1 = new NormalRandomVariable(0, v);
+		let rv2 = new NormalRandomVariable(v*v, v);
+		return rv1.cdf(u) - Math.exp(-(u + v*v)/(2 + Math.log(rv2.cdf(u))));
+	}
+	
+	logLikelihood(arrayValues) {
+		let ret = 0.0;
+		for (let v of arrayValues) {
+			ret += Math.log(this.pdf(v));
+		}
+		return ret;
+	}
+	
+	getValue() {
+		let rv1 = new NormalRandomVariable(this.mu, this.sig);
+		let rv2 = new ExponentialRandomVariable(this.lam);
+		return rv1.getValue() + rv2.getValue();
+	}
 }
 
 try {
