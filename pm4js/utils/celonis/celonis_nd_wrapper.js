a72bbdd528a43d79346cedd7e19ca08e0cfce302
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-30 13:48:10 +0200

    MVP visualization
    MVP visualization

--- pm4js/utils/celonis/celonis_nd_wrapper.js
+++ pm4js/utils/celonis/celonis_nd_wrapper.js
@@ -14,6 +14,71 @@ class CelonisNDWrapper {
 		}
 		return mvp;
 	}
+	
+	static uuidv4() {
+	  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	  });
+	}
+	
+	static nodeUuid() {
+		let uuid = CelonisNDWrapper.uuidv4();
+		return "n"+uuid.replace(/-/g, "");
+	}
+	
+	static stringToColour(str) {
+	  var hash = 0;
+	  for (var i = 0; i < str.length; i++) {
+		hash = str.charCodeAt(i) + ((hash << 5) - hash);
+	  }
+	  var colour = '#';
+	  for (var i = 0; i < 3; i++) {
+		var value = (hash >> (i * 8)) & 0xFF;
+		colour += ('00' + value.toString(16)).substr(-2);
+	  }
+	  return colour;
+	}
+	
+	static visualizationMVP(mvp) {
+		let ret = [];
+		let activities = {};
+		let startNodes = {};
+		let endNodes = {};
+		ret.push("digraph G {");
+		for (let persp in mvp) {
+			let perspCol = CelonisNDWrapper.stringToColour(persp);
+			for (let act in mvp[persp].activities) {
+				if (!(act in activities)) {
+					let nodeUuid = CelonisNDWrapper.nodeUuid();
+					activities[act] = nodeUuid;
+					ret.push(nodeUuid+" [shape=box, label=\""+act+"\"]");
+				}
+			}
+			let startNodeUuid = CelonisNDWrapper.nodeUuid();
+			startNodes[persp] = startNodeUuid;
+			ret.push(startNodeUuid+" [shape=ellipse, label=\""+persp+"\", color=\""+perspCol+"\", style=filled, fillcolor=\""+perspCol+"\"]");
+			let endNodeUuid = CelonisNDWrapper.nodeUuid();
+			endNodes[persp] = endNodeUuid;
+			ret.push(endNodeUuid+" [shape=ellipse, label=\""+persp+"\", color=\""+perspCol+"\", style=filled, fillcolor=\""+perspCol+"\"]");
+		}
+
+		for (let persp in mvp) {
+			let perspCol = CelonisNDWrapper.stringToColour(persp);
+			for (let edge0 in mvp[persp].pathsFrequency) {
+				let edge = edge0.split(",");
+				ret.push(activities[edge[0]]+" -> "+activities[edge[1]]+" [color=\""+perspCol+"\", label=\""+mvp[persp].pathsFrequency[edge0]+"\"]")
+			}
+			for (let act in mvp[persp].startActivities) {
+				ret.push(startNodes[persp]+" -> "+activities[act]+" [color=\""+perspCol+"\", label=\""+mvp[persp].startActivities[act]+"\"]");
+			}
+			for (let act in mvp[persp].endActivities) {
+				ret.push(activities[act]+" -> "+endNodes[persp]+" [color=\""+perspCol+"\", label=\""+mvp[persp].endActivities[act]+"\"]");
+			}
+		}
+		ret.push("}");
+		return ret.join("\n");
+	}
 }
 
 try {
