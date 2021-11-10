5152765ed5a40522b82bc2a2960a9fbb81ba5d1b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-11-10 14:07:33 +0100

    added algorithms
    added algorithms

--- pm4js/algo/transformation/interval_tree.js
+++ pm4js/algo/transformation/interval_tree.js
@@ -26,10 +26,76 @@ class IntervalTreeBuilder {
 	}
 }
 
+class IntervalTreeAlgorithms {
+	static resourceWorkload(tree, pointOfTime=null, resourceKey="org:resource") {
+		if (pointOfTime == null) {
+			pointOfTime = (tree.mintime + tree.maxtime) / 2.0;
+		}
+		let contained = tree.queryPoint(pointOfTime);
+		let returned = {};
+		for (let p of contained) {
+			let trace = p.value[0];
+			let idx = p.value[1];
+			let eve = trace.events[idx+1];
+			let res = eve.attributes[resourceKey].value;
+			if (!(res in returned)) {
+				returned[res] = 1;
+			}
+			else {
+				returned[res] += 1;
+			}
+		}
+		return returned;
+	}
+	
+	static targetActivityWorkload(tree, pointOfTime=null, activityKey="concept:name") {
+		if (pointOfTime == null) {
+			pointOfTime = (tree.mintime + tree.maxtime) / 2.0;
+		}
+		let contained = tree.queryPoint(pointOfTime);
+		let returned = {};
+		for (let p of contained) {
+			let trace = p.value[0];
+			let idx = p.value[1];
+			let eve = trace.events[idx+1];
+			let act = eve.attributes[activityKey].value;
+			if (!(act in returned)) {
+				returned[act] = 1;
+			}
+			else {
+				returned[act] += 1;
+			}
+		}
+		return returned;
+	}
+	
+	static sourceActivityWorkload(tree, pointOfTime=null, activityKey="concept:name") {
+		if (pointOfTime == null) {
+			pointOfTime = (tree.mintime + tree.maxtime) / 2.0;
+		}
+		let contained = tree.queryPoint(pointOfTime);
+		let returned = {};
+		for (let p of contained) {
+			let trace = p.value[0];
+			let idx = p.value[1];
+			let eve = trace.events[idx];
+			let act = eve.attributes[activityKey].value;
+			if (!(act in returned)) {
+				returned[act] = 1;
+			}
+			else {
+				returned[act] += 1;
+			}
+		}
+		return returned;
+	}
+}
+
 try {
 	require('../../pm4js.js');
-	module.exports = {IntervalTreeBuilder: IntervalTreeBuilder};
+	module.exports = {IntervalTreeBuilder: IntervalTreeBuilder, IntervalTreeAlgorithms: IntervalTreeAlgorithms};
 	global.IntervalTreeBuilder = IntervalTreeBuilder;
+	global.IntervalTreeAlgorithms = IntervalTreeAlgorithms;
 }
 catch (err) {
 	// not in node
