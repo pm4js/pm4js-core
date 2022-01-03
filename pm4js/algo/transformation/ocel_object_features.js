688d8b25871b257c9d5abea09f60fad86d1ba8f2
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-03 12:50:17 +0100

    encodeLifecycleActivities encodeLifecycleDuration encodeLifecycleLength

--- pm4js/algo/transformation/ocel_object_features.js
+++ pm4js/algo/transformation/ocel_object_features.js
@@ -67,6 +67,81 @@ class OcelObjectFeatures {
 		}
 		return {"data": data, "featureNames": featureNames};
 	}
+	
+	static getObjectsLifecycle(ocel) {
+		let lif = {};
+		let events = ocel["ocel:events"];
+		for (let evId in events) {
+			let eve = events[evId];
+			for (let objId of eve["ocel:omap"]) {
+				if (!(objId in lif)) {
+					lif[objId] = [];
+				}
+				lif[objId].push(eve);
+			}
+		}
+		return lif;
+	}
+	
+	static encodeLifecycleActivities(ocel) {
+		let events = ocel["ocel:events"];
+		let objects = ocel["ocel:objects"];
+		let diffActivities = {};
+		for (let evId in events) {
+			let eve = events[evId];
+			diffActivities[eve["ocel:activity"]] = 0;
+		}
+		diffActivities = Object.keys(diffActivities);
+		let zeroArr = [];
+		for (let act of diffActivities) {
+			zeroArr.push(0);
+		}
+		let objLifecycle = OcelObjectFeatures.getObjectsLifecycle(ocel);
+		let data = [];
+		for (let objId in objects) {
+			let lif = objLifecycle[objId];
+			let vect = [...zeroArr];
+			for (let eve of lif) {
+				vect[diffActivities.indexOf(eve["ocel:activity"])] += 1;
+			}
+			data.push(vect);
+		}
+		let featureNames = [];
+		for (let act of diffActivities) {
+			featureNames.push("@@obj_lif_act_"+act);
+		}
+		return {"data": data, "featureNames": featureNames};
+	}
+	
+	static encodeLifecycleDuration(ocel) {
+		let events = ocel["ocel:events"];
+		let objects = ocel["ocel:objects"];
+		let data = [];
+		let featureNames = ["@@obj_lif_dur"];
+		let objLifecycle = OcelObjectFeatures.getObjectsLifecycle(ocel);
+		for (let objId in objects) {
+			let lif = objLifecycle[objId];
+			let st = lif[0]["ocel:timestamp"].getTime();
+			let et = lif[lif.length - 1]["ocel:timestamp"].getTime();
+			data.push([(et-st)/1000.0]);
+		}
+		return {"data": data, "featureNames": featureNames};
+	}
+	
+	static encodeLifecycleLength(ocel) {
+		let events = ocel["ocel:events"];
+		let objects = ocel["ocel:objects"];
+		let data = [];
+		let featureNames = ["@@obj_lif_length"];
+		let objLifecycle = OcelObjectFeatures.getObjectsLifecycle(ocel);
+		for (let objId in objects) {
+			let lif = objLifecycle[objId];
+			data.push([lif.length]);
+		}
+		return {"data": data, "featureNames": featureNames};
+	}
+	
+	
 }
 
 try {
