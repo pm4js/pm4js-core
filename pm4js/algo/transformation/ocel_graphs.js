e09306465b61eb5ea60b9034b3e9c91d39fd2227
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-03 11:03:11 +0100

    object inheritance graph

--- pm4js/algo/transformation/ocel_graphs.js
+++ pm4js/algo/transformation/ocel_graphs.js
@@ -21,8 +21,8 @@ class OcelGraphs {
 		let ret = {};
 		let events = ocel["ocel:events"];
 		let objects = ocel["ocel:objects"];
-		let seenObjects = {};
 		let evIds = Object.keys(events);
+		let seenObjects = {};
 		for (let evId of evIds) {
 			let eve = events[evId];
 			for (let objId of eve["ocel:omap"]) {
@@ -47,8 +47,8 @@ class OcelGraphs {
 		let ret = {};
 		let events = ocel["ocel:events"];
 		let objects = ocel["ocel:objects"];
-		let seenObjects = {};
 		let evIds = Object.keys(events);
+		let seenObjects = {};
 		for (let evId of evIds) {
 			let eve = events[evId];
 			for (let objId of eve["ocel:omap"]) {
@@ -75,8 +75,8 @@ class OcelGraphs {
 		let ret = {};
 		let events = ocel["ocel:events"];
 		let objects = ocel["ocel:objects"];
-		let seenObjects = {};
 		let evIds = Object.keys(events).reverse();
+		let seenObjects = {};
 		for (let evId of evIds) {
 			let eve = events[evId];
 			for (let objId of eve["ocel:omap"]) {
@@ -94,6 +94,42 @@ class OcelGraphs {
 				if (!(objId in seenObjects)) {
 					seenObjects[objId] = 0;
 				}
+			}
+		}
+		return Object.keys(ret);
+	}
+	
+	static objectInheritanceGraph(ocel) {
+		let ret = {};
+		let events = ocel["ocel:events"];
+		let objects = ocel["ocel:objects"];
+		let evIds = Object.keys(events).reverse();
+		let lastEventLifecycle = {};
+		for (let evId of evIds) {
+			let eve = events[evId];
+			for (let objId of eve["ocel:omap"]) {
+				if (!(objId in lastEventLifecycle)) {
+					lastEventLifecycle[objId] = evId;
+				}
+			}
+		}
+		evIds = evIds.reverse();
+		let seenObjects = {};
+		for (let evId of evIds) {
+			let eve = events[evId];
+			for (let objId of eve["ocel:omap"]) {
+				if (objId in lastEventLifecycle && lastEventLifecycle[objId] == evId) {
+					for (let objId2 of eve["ocel:omap"]) {
+						if (objId != objId2) {
+							if (!(objId2 in seenObjects)) {
+								ret[[objId, objId2]] = 0;
+							}
+						}
+					}
+				}
+			}
+			for (let objId of eve["ocel:omap"]) {
+				seenObjects[objId] = 0;
 			}
 		}
 		return Object.keys(ret);
