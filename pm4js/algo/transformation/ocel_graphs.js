b4c68f3c9b46591398c200696f698add11625055
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-03 10:48:18 +0100

    cobirth/codeath graphs

--- pm4js/algo/transformation/ocel_graphs.js
+++ pm4js/algo/transformation/ocel_graphs.js
@@ -3,7 +3,8 @@ class OcelGraphs {
 		let ret = {};
 		let events = ocel["ocel:events"];
 		let objects = ocel["ocel:objects"];
-		for (let evId in events) {
+		let evIds = Object.keys(events);
+		for (let evId of evIds) {
 			let eve = events[evId];
 			for (let objId of eve["ocel:omap"]) {
 				for (let objId2 of eve["ocel:omap"]) {
@@ -21,13 +22,70 @@ class OcelGraphs {
 		let events = ocel["ocel:events"];
 		let objects = ocel["ocel:objects"];
 		let seenObjects = {};
-		for (let evId in events) {
+		let evIds = Object.keys(events);
+		for (let evId of evIds) {
 			let eve = events[evId];
 			for (let objId of eve["ocel:omap"]) {
 				if (objId in seenObjects) {
 					for (let objId2 of eve["ocel:omap"]) {
 						if (!(objId2 in seenObjects)) {
 							ret[[objId, objId2]] = 0;
+						}
+					}
+				}
+			}
+			for (let objId of eve["ocel:omap"]) {
+				if (!(objId in seenObjects)) {
+					seenObjects[objId] = 0;
+				}
+			}
+		}
+		return Object.keys(ret);
+	}
+	
+	static objectCobirthGraph(ocel) {
+		let ret = {};
+		let events = ocel["ocel:events"];
+		let objects = ocel["ocel:objects"];
+		let seenObjects = {};
+		let evIds = Object.keys(events);
+		for (let evId of evIds) {
+			let eve = events[evId];
+			for (let objId of eve["ocel:omap"]) {
+				if (!(objId in seenObjects)) {
+					for (let objId2 of eve["ocel:omap"]) {
+						if (!(objId2 in seenObjects)) {
+							if (objId != objId2) {
+								ret[[objId, objId2]] = 0;
+							}
+						}
+					}
+				}
+			}
+			for (let objId of eve["ocel:omap"]) {
+				if (!(objId in seenObjects)) {
+					seenObjects[objId] = 0;
+				}
+			}
+		}
+		return Object.keys(ret);
+	}
+	
+	static objectCodeathGraph(ocel) {
+		let ret = {};
+		let events = ocel["ocel:events"];
+		let objects = ocel["ocel:objects"];
+		let seenObjects = {};
+		let evIds = Object.keys(events).reverse();
+		for (let evId of evIds) {
+			let eve = events[evId];
+			for (let objId of eve["ocel:omap"]) {
+				if (!(objId in seenObjects)) {
+					for (let objId2 of eve["ocel:omap"]) {
+						if (!(objId2 in seenObjects)) {
+							if (objId != objId2) {
+								ret[[objId, objId2]] = 0;
+							}
 						}
 					}
 				}
