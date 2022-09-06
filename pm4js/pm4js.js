7204c05459f82a5b890d221dca0fcff0f5f38c98
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-09-06 10:46:20 +0200

    adding algorithm observer mechanism

--- pm4js/pm4js.js
+++ pm4js/pm4js.js
@@ -1,6 +1,13 @@
 class Pm4JS {
 	// version >= 0.0.20: license BSD-3-Clause
 	
+	static uuidv4() {
+	  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	  });
+	}
+	
 	static registerObject(obj, description) {
 		if (Pm4JS.registrationEnabled) {
 			if (description == null) {
@@ -59,6 +66,52 @@ class Pm4JS {
 	static registerCallback(f) {
 		Pm4JS.objectsCallbacks.push(f);
 	}
+	
+	static startAlgorithm(details) {
+		let uuid = Pm4JS.uuidv4();
+		Pm4JS.runningAlgorithms[uuid] = details;
+		for (let obs of Pm4JS.runningAlgorithmsObservers) {
+			obs.algorithmStarted(uuid, details);
+		}
+		return uuid;
+	}
+	
+	static updateAlgorithmStatus(uuid, details) {
+		let oldDetails = Pm4JS.runningAlgorithms[uuid];
+		Pm4JS.runningAlgorithms[uuid] = details;
+		for (let obs of Pm4JS.runningAlgorithmsObservers) {
+			obs.algorithmStatusUpdated(uuid, oldDetails, details);
+		}
+		return uuid;
+	}
+	
+	static stopAlgorithm(uuid, details) {
+		for (let obs of Pm4JS.runningAlgorithmsObservers) {
+			obs.algorithmStopped(uuid, details);
+		}
+		delete Pm4JS.runningAlgorithms[uuid];
+	}
+	
+	static registerObserver(observer) {
+		Pm4JS.runningAlgorithmsObservers.push(observer);
+	}
+}
+
+class Pm4JSObserverExample {
+	algorithmStarted(uuid, details) {
+		console.log("algorithm started! " + uuid);
+		console.log(details);
+	}
+	
+	algorithmStatusUpdated(uuid, oldDetails, details) {
+		console.log("algorithm status updated! " + uuid);
+		console.log(details);
+	}
+	
+	algorithmStopped(uuid, details) {
+		console.log("algorithm stopped! " + uuid);
+		console.log(details);
+	}
 }
 
 Pm4JS.VERSION = "0.0.28";
@@ -69,10 +122,13 @@ Pm4JS.importers = [];
 Pm4JS.exporters = [];
 Pm4JS.visualizers = [];
 Pm4JS.objectsCallbacks = [];
+Pm4JS.runningAlgorithms = {};
+Pm4JS.runningAlgorithmsObservers = [];
 
 try {
-	module.exports = {Pm4JS: Pm4JS};
+	module.exports = {Pm4JS: Pm4JS, Pm4JSObserverExample: Pm4JSObserverExample};
 	global.Pm4JS = Pm4JS;
+	global.Pm4JSObserverExample = Pm4JSObserverExample;
 }
 catch (err) {
 	// not in node
